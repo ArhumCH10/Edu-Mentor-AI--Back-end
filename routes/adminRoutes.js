6fda3cbbc9d716acbcbd8d916bb435b41d6cf668
Bilal Mumtaz <bilalmumtaz868@gmail.com>
2024-01-28 13:56:18 +0500

    admin verifies user

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const { body } = require("express-validator");
 const Teacher = require("../models/teacherSchema");
 router.get("/get-all-users", async (req, res) => {
+  console.log("sending users");
   try {
     // Fetch all users from the database
     const users = await Teacher.find();
@@ -11,6 +12,31 @@ router.get("/get-all-users", async (req, res) => {
   } catch (error) {
     console.error("Error fetching users:", error);
     res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+router.post("/verify-teacher/:id", async (req, res) => {
+  try {
+    const teacherId = req.params.id;
+
+    // Find the teacher by ID
+    const teacher = await Teacher.findById(teacherId);
+
+    if (!teacher) {
+      return res.status(404).json({ error: "Teacher not found" });
+    }
+
+    // Update the isVerified property to true
+    teacher.isVerified = true;
+
+    // Save the updated teacher
+    await teacher.save();
+
+    console.log("teacher verified");
+    return res.json({ message: "Teacher verified successfully" });
+  } catch (error) {
+    console.error("Error verifying teacher:", error);
+    return res.status(500).json({ error: "Internal Server Error" });
   }
 });
 
