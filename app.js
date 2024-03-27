9bcd860c477c24d5244dc8431b1183497fde7471
GhousCodeX <ghousali002@gmail.com>
2024-03-27 23:34:12 +0500

    Search Feature done & Backend data show frontEnd except schedule and searchByEmail

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const adminRoutes = require("./routes/adminRoutes");
 const studentRoutes = require("./routes/studentRoutes");
 const path = require("path");
 const URL =
-  "mongodb+srv://supremebilal78:t5OxJKSK26h9q9YU@test-db.v6p1fbj.mongodb.net/";
+  "mongodb+srv://ghous:12345@cluster0.xaycdod.mongodb.net/";
 
 const app = express();
 
--- routes/teacherRoutes.js
+++ routes/teacherRoutes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const teacherInfo = require('../controllers/teacherInfo');
 const upload = require('../multerConfig');
 const videoUpload = require('../multerConfig');
+const { ObjectId } = require('mongoose').Types;
 const path = require('path');
 const fs = require('fs');
 const teacherDB = require("../models/teacherSchema");
@@ -62,8 +63,8 @@ router.get('/video/:userId', async (req, res) => {
         const thumb = user.video.thumbnail;
         const videoString = video.join(', ');
         const thumbString = thumb ? thumb.join(', ') : null;
-        console.log('video: ',videoString);
-        console.log('Thumb',thumbString);
+        console.log('video: ', videoString);
+        console.log('Thumb', thumbString);
         // Concatenate directory path with video and thumbnail paths
         // const videoData = {
         //     data: path.join(__dirname, '..', videoString), // Assuming __dirname is the directory of this file
@@ -74,7 +75,7 @@ router.get('/video/:userId', async (req, res) => {
             thumbnail: thumbString,
         };
         console.log(user.video);
-     
+
 
         res.status(200).json({
             message: 'Video data retrieved successfully',
@@ -89,19 +90,103 @@ router.get('/video/:userId', async (req, res) => {
 router.get('/totalTutor/:subject', async (req, res) => {
     const { subject } = req.params;
 
-  try {
-    console.log('subject:', subject);
-    const totalTutorCount = await teacherDB.countDocuments({ subjectsTaught: subject });
-    console.log('totalTutorCount:', totalTutorCount);
-    res.status(200).json({ 
-        message: 'Total Tutor related subject retrieved successfully',
-        totalTutorCount: totalTutorCount 
-    });
-  } catch (error) {
-    console.error('Error fetching total tutor count:', error);
-    res.status(500).json({ error: 'Internal Server Error' });
-  }
+    try {
+        console.log('subject:', subject);
+        const totalTutorCount = await teacherDB.countDocuments({ subjectsTaught: subject });
+        console.log('totalTutorCount:', totalTutorCount);
+        res.status(200).json({
+            message: 'Total Tutor related subject retrieved successfully',
+            totalTutorCount: totalTutorCount
+        });
+    } catch (error) {
+        console.error('Error fetching total tutor count:', error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
 
+});
+
+router.get('/searchTutors', async (req, res) => {
+    try {
+        const { subject, Country, Days, Times, minP, maxP } = req.query;
+
+        console.log("data in query", subject, Country, Days, Times, minP, maxP);
+        const query = {};
+        if (subject) query.subjectsTaught = subject;
+        if (Days) {
+            const daysArray = Days.split('+').map(day => {
+                switch (day) {
+                    case 'Sun':
+                        return 'Sunday';
+                    case 'Mon':
+                        return 'Monday';
+                    case 'Tues':
+                        return 'Tuesday';
+                    case 'Wed':
+                        return 'Wednesday';
+                    case 'Thurs':
+                        return 'Thursday';
+                    case 'Fri':
+                        return 'Friday';
+                    case 'Sat':
+                        return 'Saturday';
+                    default:
+                        return '';
+                }
+            });
+            query['availability.day'] = daysArray;
+        }
+        if (Times) {
+            const timeRanges = Times.split('+').map(timeRange => {
+                const [start, end] = timeRange.split('-').map(time => {
+                    if (time.includes(':')) {
+                        return time;
+                    } else {
+                        const hour = Number(time);
+                        return `${hour.toString().padStart(2, '0')}:00`;
+                    }
+                });
+               return { 'availability.slots.from': { $gte: start }, 'availability.slots.to': { $lte: end } };
+            });
+            console.log("timeRanges", timeRanges);
+            query['$or'] = timeRanges;
+        }
+        if (Country) {
+            const countriesArray = Country.split('+').map(country => country.toLowerCase());
+            query.countryOrigin = countriesArray;
+        }
+        if (minP && maxP) {
+            query.hourlyPriceUSD = { $gte: minP, $lte: maxP };
+        }
+        query.registrationCompleted = true;
+        query.isVerified = true;
+        query.isRegistered = true;
+
+        console.log("query", query);
+
+        const tutors = await teacherDB.find(query).select('-password -isVerified -verificationToken -isRegistered -registrationCompleted -isGreaterThan18');
+        console.log("tutors", tutors);
+        res.json(tutors);
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ message: 'Internal server error' });
+    }
+});
+
+router.get('/tutorProfile/', async (req, res) => {
+    try {
+        const {id} = req.query;
+        console.log("id: ", id); 
+        const tutorObjectId = new ObjectId(id);
+        console.log("tutorObjectId: ", tutorObjectId);
+        const tutor = await teacherDB.findById(tutorObjectId).select('-password -isVerified -verificationToken -isRegistered -registrationCompleted -isGreaterThan18');
+        if (!tutor) {
+            return res.status(404).json({ message: 'Tutor not found' });
+        }
+        res.status(200).json(tutor);
+    } catch (error) {
+        console.error('Error fetching tutor data by ID:', error);
+        res.status(500).json({ message: 'Internal Server Error' });
+    }
 });
 module.exports = router;
 
